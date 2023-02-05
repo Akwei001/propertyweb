5b68da41823c15fb01401d259b6f210be14710be
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2023-02-05 15:36:40 +0000

    components setup

---
+++ components/index.ts
@@ -0,0 +1,5 @@
+export { default as Banner } from './Banner';
+export { default as Footer } from './Footer';
+export { default as Navbar } from './Navbar';
+export { default as Properties } from './Properties';
+export { default as Layout } from './Layout';
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,12 @@
 export default function Home() {
   return (
-    <main>
-      <h1 className='text-2xl'>Hello World!</h1>
-    </main>
+    <>
+      <div>Navbar</div>
+      <div>Banner</div>
+      <div>
+        {['Property 1', 'Property 2', 'Property 3'].map((property) => property)}
+      </div>
+      <div>Footer</div>
+    </>
   );
 }

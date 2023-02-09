5e003011c13a54a006c7d1c528460c580ad26722
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2023-02-09 09:58:56 +0000

    navbar added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,15 @@
-export default function Home() {
+import React from 'react';
+// import Head from 'next/head';
+import { groq } from 'next-sanity';
+import { getClient } from 'lib/sanity';
+import { Banner, Footer, Navbar, Properties, Layout } from 'components';
+
+function Home({ bannerData }) {
   return (
     <>
       <div>Navbar</div>
-      <div>Banner</div>
+      <Banner />
+      {console.log(bannerData)}
       <div>
         {['Property 1', 'Property 2', 'Property 3'].map((property) => property)}
       </div>
@@ -10,3 +17,14 @@ export default function Home() {
     </>
   );
 }
+
+export const getServerSideProps = async () => {
+  const Bannerquery = groq`*[_type == 'banner']`;
+  const bannerData = await getClient().fetch(Bannerquery);
+
+  return {
+    props: { bannerData },
+  };
+};
+
+export default Home;

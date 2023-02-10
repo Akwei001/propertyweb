388e9f367777f27ecc01322536a900223223d596
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2023-02-10 11:10:36 +0000

    banner data in ui

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,39 +1,33 @@
 import React from 'react';
+import Link from 'next/link';
+import urlFor from '../../lib/urlFor';
 
-const Banner = () => {
+const Banner = ({ banner }) => {
   return (
     <>
-      <section className='relative bg-[url(https://images.unsplash.com/photo-1604014237800-1c9102c219da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80)] bg-cover bg-center bg-no-repeat'>
+      <section className='relative bg-[<img src={urlFor(banner.image)} />] bg-cover bg-center bg-no-repeat'>
+        {/* {<img src={urlFor(banner.image)} />} */}
         <div className='absolute inset-0 bg-white/75 sm:bg-transparent sm:bg-gradient-to-r sm:from-white/95 sm:to-white/25'></div>
 
-        <div className='relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8'>
+        <div className='relative max-w-screen-xl px-4 py-32 mx-auto sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8'>
           <div className='max-w-xl text-center sm:text-left'>
             <h1 className='text-3xl font-extrabold sm:text-5xl'>
-              Let us find your
-              <strong className='block font-extrabold text-rose-700'>
-                Forever Home.
-              </strong>
+              {banner.largeText1}
+              <strong className='block font-extrabold text-rose-700'></strong>
             </h1>
 
-            <p className='mt-4 max-w-lg sm:text-xl sm:leading-relaxed'>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt
-              illo tenetur fuga ducimus numquam ea!
-            </p>
+            <p className='max-w-lg mt-4 sm:text-xl sm:leading-relaxed'></p>
 
-            <div className='mt-8 flex flex-wrap gap-4 text-center'>
-              <a
-                href='#'
-                className='block w-full rounded bg-rose-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-rose-700 focus:outline-none focus:ring active:bg-rose-500 sm:w-auto'
-              >
-                Get Started
-              </a>
+            <div className='flex flex-wrap gap-4 mt-8 text-center'>
+              <Link href={'#'}>
+                <button className='block w-full px-12 py-3 text-sm font-medium text-white rounded shadow bg-rose-600 hover:bg-rose-700 focus:outline-none focus:ring active:bg-rose-500 sm:w-auto'>
+                  {banner.buttonText}
+                </button>
+              </Link>
 
-              <a
-                href='#'
-                className='block w-full rounded bg-white px-12 py-3 text-sm font-medium text-rose-600 shadow hover:text-rose-700 focus:outline-none focus:ring active:text-rose-500 sm:w-auto'
-              >
+              <button className='block w-full px-12 py-3 text-sm font-medium bg-white rounded shadow text-rose-600 hover:text-rose-700 focus:outline-none focus:ring active:text-rose-500 sm:w-auto'>
                 Learn More
-              </a>
+              </button>
             </div>
           </div>
         </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Head from 'next/head';
-import { groq } from 'next-sanity';
+// import Head from 'next/head';
+// import { groq } from 'next-sanity';
 // import { getClient } from 'lib/sanity';
 import { client } from '../../lib/sanity.client';
 import { GetStaticProps, GetStaticPaths, GetServerSideProps } from 'next';
@@ -11,7 +11,7 @@ export default function Home({ bannerData }) {
     <>
       <Header />
       {console.log(bannerData)}
-      <Banner />
+      <Banner banner={bannerData.length && bannerData[0]} />
       <Properties />
       <h2 className='text-2xl text-cyan-500'>Start</h2>
       <Footer />

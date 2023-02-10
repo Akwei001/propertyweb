ac98e87411a4fb7255730e15b37cddabd1ecbf2c
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2023-02-10 21:09:03 +0000

    placeholder components added

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,40 +1,41 @@
 import React from 'react';
-import Link from 'next/link';
-import urlFor from '../../lib/urlFor';
 
-const Banner = ({ banner }) => {
+const Banner = () => {
   return (
-    <>
-      <section className='relative bg-[<img src={urlFor(banner.image)} />] bg-cover bg-center bg-no-repeat'>
-        {/* {<img src={urlFor(banner.image)} />} */}
-        <div className='absolute inset-0 bg-white/75 sm:bg-transparent sm:bg-gradient-to-r sm:from-white/95 sm:to-white/25'></div>
-
-        <div className='relative max-w-screen-xl px-4 py-32 mx-auto sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8'>
-          <div className='max-w-xl text-center sm:text-left'>
-            <h1 className='text-3xl font-extrabold sm:text-5xl'>
-              {banner.largeText1}
-              <strong className='block font-extrabold text-rose-700'></strong>
+    <div>
+      <section className='text-gray-600 body-font'>
+        <div className='container flex flex-col items-center px-5 py-24 mx-auto md:flex-row'>
+          <div className='flex flex-col items-center mb-16 text-center lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 md:items-start md:text-left md:mb-0'>
+            <h1 className='mb-4 text-3xl font-medium text-gray-900 title-font sm:text-4xl'>
+              Before they sold out
+              <br className='hidden lg:inline-block' />
+              readymade gluten
             </h1>
-
-            <p className='max-w-lg mt-4 sm:text-xl sm:leading-relaxed'></p>
-
-            <div className='flex flex-wrap gap-4 mt-8 text-center'>
-              <Link href={'#'}>
-                <button className='block w-full px-12 py-3 text-sm font-medium text-white rounded shadow bg-rose-600 hover:bg-rose-700 focus:outline-none focus:ring active:bg-rose-500 sm:w-auto'>
-                  {banner.buttonText}
-                </button>
-              </Link>
-
-              <button className='block w-full px-12 py-3 text-sm font-medium bg-white rounded shadow text-rose-600 hover:text-rose-700 focus:outline-none focus:ring active:text-rose-500 sm:w-auto'>
-                Learn More
+            <p className='mb-8 leading-relaxed'>
+              Copper mug try-hard pitchfork pour-over freegan heirloom neutra
+              air plant cold-pressed tacos poke beard tote bag. Heirloom echo
+              park mlkshk tote bag selvage hot chicken authentic tumeric
+              truffaut hexagon try-hard chambray.
+            </p>
+            <div className='flex justify-center'>
+              <button className='inline-flex px-6 py-2 text-lg text-white bg-indigo-500 border-0 rounded focus:outline-none hover:bg-indigo-600'>
+                Button
+              </button>
+              <button className='inline-flex px-6 py-2 ml-4 text-lg text-gray-700 bg-gray-100 border-0 rounded focus:outline-none hover:bg-gray-200'>
+                Button
               </button>
             </div>
           </div>
+          <div className='w-5/6 lg:max-w-lg lg:w-full md:w-1/2'>
+            <img
+              className='object-cover object-center rounded'
+              alt='hero'
+              src='https://dummyimage.com/720x600'
+            />
+          </div>
         </div>
       </section>
-
-      <h2>This is a banner</h2>
-    </>
+    </div>
   );
 };
 
